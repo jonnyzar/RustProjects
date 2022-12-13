9cf9f1357daad0408a66fac028c4bf4744190342
jonnyzar <64309336+jonnyzar@users.noreply.github.com>
2022-12-13 22:12:31 +0100

    mutable references

--- references/src/main.rs
+++ references/src/main.rs
@@ -11,6 +11,27 @@ fn main() {
     change(&mut s_change);
 
     println!("{s_change}");
+
+// creating multiple mutable varibles
+    {
+        let _k = &mut s_change;
+    }
+
+    let _f = &mut s_change;
+
+// but for immutable no need to set scope
+
+    let mut x = 5;
+    
+    let ref_x = &x; //immutable reference of a mutable variable
+    let ref_xx = &x; 
+
+    println!("{x}, {ref_x} and {ref_xx}"); // scope of immutable refs end when the println is used
+
+    let ref_yyy = &mut x;
+
+    println!("{ref_yyy}");
+
 }
 
 fn change (s: &mut String) {
@@ -20,3 +41,6 @@ fn change (s: &mut String) {
 fn calc_length(s: &String) -> usize { //func accepts some reference to a string
     s.len() // borrowing
 }// s goes out of scope but is not dropped
+
+
+
