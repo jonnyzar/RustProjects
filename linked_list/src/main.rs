a378e17bfc084a3f7d19596c85a77dd2bde60245
jonnyzar <64309336+jonnyzar@users.noreply.github.com>
2023-03-08 22:14:16 +0100

    linked list

--- linked_list/src/main.rs
+++ linked_list/src/main.rs
@@ -1,18 +1,52 @@
-//generates an implementation of the Clone trait for a struct or enum.
+use crate::address::Nil;
 
+//generates an implementation of the Clone trait for a struct or enum.
 #[derive(Clone)]
 enum address{
-    address(Box<myList>),
+    address(Box<Node>),
     Nil,
 }
 
 #[derive(Clone)]
-struct myList{
+struct Node{
     value: u32,
     next: address,
 }
 
+impl Node{
+
+    //create a node
+    fn new(value: u32) -> Self {
+        Self { 
+            value: value,
+            next: Nil,
+         }
+    }
+
+    //add a new node to the end of the existing
+
+    fn append(&mut self, next_value: u32) {
+
+        // create new node an point it to Nil
+        let next_node = Node {
+            value: next_value,
+            next: address::Nil,
+        };
+
+        //point current node to new node
+        self.next = address::address(Box::new(next_node))
+
+    }
+
+
+}
 
 fn main(){
     
+    let my_list = Node::new(732);
+
+    assert_eq!(732, my_list.value);
+
+    println!("{}", my_list.value);
+   
 }
